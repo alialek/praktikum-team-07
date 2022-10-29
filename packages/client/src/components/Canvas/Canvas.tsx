780f5d666550e83732df436f157f00a41ef95fd0
Sergey Vlasov <sidodji1337@yandex.ru>
2022-10-29 08:05:39 +0300

    feat: Добаил компонент Canvas и хук useCanvas. Установил необходимые зависимости.

---
+++ packages/client/src/components/Canvas/Canvas.tsx
@@ -0,0 +1,38 @@
+import React, { MutableRefObject } from 'react';
+import {
+    Button, Grid,
+} from '@mui/material';
+import ReplayIcon from '@mui/icons-material/Replay';
+import { useCanvas } from '@/hooks/useCanvas'
+
+const CanvasComponent = () => {
+    const [canvasRef, isRefreshed, setRefreshed] = useCanvas();
+
+    const handleRefreshCanvas = () => {
+        setRefreshed(!isRefreshed);
+    };
+
+    return (
+        <Grid container spacing={2} justifyContent="center" alignItems="center" direction='column'>
+            <Grid item xs={12}>
+                <canvas
+                    ref={canvasRef as MutableRefObject<HTMLCanvasElement>}
+                    width={736}
+                    height={472}
+                />
+            </Grid>
+            <Grid item xs={12}>
+                <Button
+                    variant='outlined'
+                    color='secondary'
+                    onClick={handleRefreshCanvas}
+                    startIcon={<ReplayIcon />}
+                >
+                    Играть еще раз
+                </Button>
+            </Grid>
+        </Grid>
+    );
+};
+
+export default CanvasComponent;
