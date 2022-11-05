0a3ace694e2e921cb90eec8503315f9ea629da3d
Sergey Vlasov <sidodji1337@yandex.ru>
2022-11-05 12:55:57 +0300

    PRA-31 - фикс константы

--- packages/client/src/сonstants/main.ts
+++ packages/client/src/сonstants/main.ts
@@ -1,2 +1,2 @@
 export const API_URL = 'https://ya-praktikum.tech/api/v2';
-export const GET_AVATAR_URL = 'https://ya-praktikum.tech/api/v2/resources';
+export const GET_AVATAR_URL = `${API_URL}/resources`;
