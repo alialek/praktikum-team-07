2a2e20f3f5c67aed722953f42ffbcedcacbbe215
Ekaterina <ches.ekaterina2010@yandex.ru>
2022-11-07 14:56:54 +0300

    added common validation file

--- packages/client/src/pages/SigninPage/SigninPage.tsx
+++ packages/client/src/pages/SigninPage/SigninPage.tsx
@@ -3,112 +3,101 @@ import { SignupPagePath } from '@/router/paths';
 import { SigninInputModel } from '../../models/auth.model'
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from "yup";
 import {Link, Button, TextField, Typography, Card, Box, Grid, Stack, CardContent, CardActions} from '@mui/material'
-import { REQUIRED_VALUE_MESSAGE, EMAIL_VALUE_MESSAGE } from "@/сonstants/main";
-
-
-const schema = yup.object({
-    login: yup.string().email().required(),
-    password: yup.string().required(),
-  }).required();
+import { REQUIRED_VALUE_MESSAGE, EMAIL_VALUE_MESSAGE } from "@/сonstants/validationMessages";
+import * as FormValidate from '../../utils/formValidation'
 
 
 export const SigninPage: React.FC = () => {
   const { register, handleSubmit, formState: { errors } } = useForm<SigninInputModel>({
-    resolver: yupResolver(schema)
+    resolver: yupResolver(FormValidate.loginFormValidationSchema)
   });
   const onSubmit = (data: SigninInputModel) => console.log(data);
   return (
-
+    
     <Box 
       component="div"
-      sx={{maxWidth: 552, ml: 'auto', mr: 'auto'}}
+      sx={{maxWidth: 552, ml: 'auto', mr: 'auto', transform: 'scale(0.8)'}}
     >
-      <Box 
-        component="span"
-        sx={{ display: 'inline-block', transform: 'scale(0.8)'}}
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: '100vh'}}
       >
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: '100vh' }}
-        >
-          <Grid item xs={3}>
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <Card sx={{ minWidth: 552, padding: '50px 40px', borderRadius: '10px'}}>
-                
-                <CardContent>
-                  <Typography 
-                    fontSize={24} 
-                    fontWeight="600" 
-                    padding="0 0 24px 0"
-                    align="center">
-                    Вход
-                  </Typography>
-            
-                  <Stack direction="column" spacing={4}>
-                    <TextField             
-                      {...register("login")}
-                      id="email"
-                      label="Email"
-                      placeholder="Email"
-                      autoFocus
-                      variant="outlined"
-                      error={!!errors?.login}
-                      helperText={errors.login && EMAIL_VALUE_MESSAGE}
-                      />
+        <Grid item xs={3}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Card sx={{ minWidth: 552, padding: '50px 40px', borderRadius: '10px', backgroundColor: '#FAFAFA'}}>
+              
+              <CardContent>
+                <Typography 
+                  fontSize={24} 
+                  fontWeight="600" 
+                  padding="0 0 24px 0"
+                  align="center">
+                  Вход
+                </Typography>
+          
+                <Stack direction="column" spacing={4}>
+                  <TextField             
+                    {...register("login")}
+                    id="email"
+                    label="Email"
+                    placeholder="Email"
+                    autoFocus
+                    variant="outlined"
+                    error={!!errors?.login}
+                    helperText={errors.login && EMAIL_VALUE_MESSAGE}
+                    />
 
-                    <TextField
-                      type="password"
-                      id="outlined-textarea"
-                      label="Пароль"
-                      placeholder="Пароль"
-                      error={!!errors?.password}
-                      helperText={errors.password && REQUIRED_VALUE_MESSAGE}
-                      multiline 
-                      
-                      {...register("password")}/>
+                  <TextField
+                    type="password"
+                    id="outlined-textarea"
+                    label="Пароль"
+                    placeholder="Пароль"
+                    error={!!errors?.password}
+                    helperText={errors.password && REQUIRED_VALUE_MESSAGE}
+                    multiline 
+                    
+                    {...register("password")}/>
 
-  
-                      <Typography
-                        variant="caption"
-                        fontSize={18} 
-                        fontWeight="500"
-                        align="center"
-                      >
-                        <Link 
-                          href={SignupPagePath.path}
-                          underline="none">
-                          Нет аккаунта? Регистрация</Link>
-                        
-                      </Typography>
-                  
-                  </Stack>
-                </CardContent>
-                <CardActions>
-                  <Button 
-                    type="submit"
-                    size="large" 
-                    variant="contained"
-                    fullWidth
-                      sx={{
-                        borderRadius: '10px'}}
+
+                    <Typography
+                      variant="caption"
+                      fontSize={18} 
+                      fontWeight="500"
+                      align="center"
                     >
-                      Войти
-                  </Button>
-                </CardActions>
+                      <Link 
+                        href={SignupPagePath.path}
+                        underline="none">
+                        Нет аккаунта? Регистрация</Link>
+                      
+                    </Typography>
                 
-              </Card>  
-            </form>
-          </Grid>
+                </Stack>
+              </CardContent>
+              <CardActions>
+                <Button 
+                  type="submit"
+                  size="large" 
+                  variant="contained"
+                  fullWidth
+                    sx={{
+                      borderRadius: '10px'}}
+                  >
+                    Войти
+                </Button>
+              </CardActions>
+              
+            </Card>  
+          </form>
         </Grid>
-      </Box>
+      </Grid>
     </Box>
- 
+   
 
   )
 }
--- packages/client/src/сonstants/main.ts
+++ packages/client/src/сonstants/main.ts
@@ -2,8 +2,3 @@ export const API_URL = 'https://ya-praktikum.tech/api/v2'
 
 export const PHONE_REGEX = /^(\+7|7|8)?[\s\\-]?\(?[489][0-9]{2}\)?[\s\\-]?[0-9]{3}[\s\\-]?[0-9]{2}[\s\\-]?[0-9]{2}$/;
 
-export const REQUIRED_VALUE_MESSAGE = 'Обязательное поле'
-export const MIN_MESSAGE = 'Минимальное количество знаков - '
-export const MAX_MESSAGE = 'Максимальное количество знаков - '
-export const PHONE_VALUE_MESSAGE = 'Телефонный номер некорректен'
-export const EMAIL_VALUE_MESSAGE = 'Почта некорректна'
