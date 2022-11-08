17196c2795d36ea8c54a8fa0034aca22b47ee547
alina <denisova@logema.org>
2022-11-08 21:30:09 +0300

    PRA-25 Привела поля форм к общему виду

--- packages/client/src/pages/SigninPage/SigninPage.tsx
+++ packages/client/src/pages/SigninPage/SigninPage.tsx
@@ -7,7 +7,6 @@ import {
   TextField,
   Typography,
   Card,
-  Box,
   Grid,
   Stack,
   CardContent,
@@ -28,90 +27,73 @@ export const SigninPage: React.FC = () => {
   });
   const onSubmit = (data: SigninInputModel) => console.log(data);
   return (
-    <Box
-      component="div"
-      sx={{ maxWidth: 552, ml: 'auto', mr: 'auto', transform: 'scale(0.8)' }}
+    <Grid
+      container
+      spacing={0}
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+      style={{ minHeight: '100vh' }}
     >
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justifyContent="center"
-        style={{ minHeight: '100vh' }}
+      <Card
+        sx={{
+          maxWidth: '552px',
+          padding: '50px 40px',
+          background: '#FAFAFA',
+          borderRadius: '10px',
+          width: '100%',
+        }}
       >
-        <Grid item xs={3}>
-          <form onSubmit={handleSubmit(onSubmit)}>
-            <Card
+        <Typography fontSize={24} fontWeight="600" padding="0 0 24px 0" align="center">
+          Вход
+        </Typography>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <CardContent>
+            <Stack direction="column" spacing={2}>
+              <TextField
+                type="text"
+                {...register('email')}
+                id="email"
+                label="Email"
+                placeholder="Email"
+                autoFocus
+                error={!!errors?.email}
+                helperText={errors.email?.message}
+              />
+
+              <TextField
+                type="password"
+                id="outlined-textarea"
+                label="Пароль"
+                placeholder="Пароль"
+                error={!!errors?.password}
+                helperText={errors.password?.message}
+                {...register('password')}
+              />
+
+              <Typography variant="caption" fontSize={15} fontWeight={700} align="center">
+                <Link href={SignupPagePath.path} underline="none">
+                  Нет аккаунта? Регистрация
+                </Link>
+              </Typography>
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Button
+              variant="contained"
+              type="submit"
+              size="medium"
+              fullWidth
+              disabled={!isValid}
               sx={{
-                minWidth: 552,
-                padding: '50px 40px',
                 borderRadius: '10px',
-                backgroundColor: '#FAFAFA',
               }}
             >
-              <CardContent>
-                <Typography
-                  fontSize={24}
-                  fontWeight="600"
-                  padding="0 0 24px 0"
-                  align="center"
-                >
-                  Вход
-                </Typography>
-
-                <Stack direction="column" spacing={4}>
-                  <TextField
-                    {...register('email')}
-                    id="email"
-                    label="Email"
-                    placeholder="Email"
-                    autoFocus
-                    variant="outlined"
-                    error={!!errors?.email}
-                    helperText={errors.email?.message}
-                  />
-
-                  <TextField
-                    type="password"
-                    id="outlined-textarea"
-                    label="Пароль"
-                    placeholder="Пароль"
-                    error={!!errors?.password}
-                    helperText={errors.password?.message}
-                    {...register('password')}
-                  />
-
-                  <Typography
-                    variant="caption"
-                    fontSize={18}
-                    fontWeight="500"
-                    align="center"
-                  >
-                    <Link href={SignupPagePath.path} underline="none">
-                      Нет аккаунта? Регистрация
-                    </Link>
-                  </Typography>
-                </Stack>
-              </CardContent>
-              <CardActions>
-                <Button
-                  variant="contained"
-                  type="submit"
-                  size="medium"
-                  fullWidth
-                  disabled={!isValid}
-                  sx={{
-                    borderRadius: '10px',
-                  }}
-                >
-                  Войти
-                </Button>
-              </CardActions>
-            </Card>
-          </form>
-        </Grid>
-      </Grid>
-    </Box>
+              Войти
+            </Button>
+          </CardActions>
+        </form>
+      </Card>
+    </Grid>
   );
 };
--- packages/client/src/pages/SignupPage/SignupPage.tsx
+++ packages/client/src/pages/SignupPage/SignupPage.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
-import { Button, TextField, Typography, Grid, Link } from '@mui/material';
+import {
+  Button,
+  TextField,
+  Typography,
+  Grid,
+  Link,
+  Card,
+  CardContent,
+  CardActions,
+  Stack,
+} from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { signupFormValidationSchema } from '@/utils/formValidation';
@@ -31,108 +41,101 @@ export const SignupPage: React.FC = () => {
         minHeight: '100vh',
       }}
     >
-      <Grid
+      <Card
         sx={{
-          maxWidth: '709px',
+          maxWidth: '552px',
           padding: '50px 40px',
           background: '#FAFAFA',
           borderRadius: '10px',
-          width: '80%',
+          width: '100%',
         }}
       >
         <Typography variant="h1" fontSize="24px" align="center" mb={2} fontWeight="600">
           Регистрация
         </Typography>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <Grid item mb={2}>
-            <TextField
-              type="text"
-              label="Имя"
-              {...register('first_name')}
-              error={!!errors?.first_name}
-              helperText={errors.first_name?.message}
-              fullWidth
-              autoFocus
-            />
-          </Grid>
+          <CardContent>
+            <Stack direction="column" spacing={2}>
+              <TextField
+                type="text"
+                label="Имя"
+                {...register('first_name')}
+                error={!!errors?.first_name}
+                helperText={errors.first_name?.message}
+                fullWidth
+                autoFocus
+              />
 
-          <Grid item mb={2}>
-            <TextField
-              type="text"
-              label="Фамилия"
-              {...register('second_name')}
-              error={!!errors.second_name}
-              helperText={errors.second_name?.message}
-              fullWidth
-            />
-          </Grid>
+              <TextField
+                type="text"
+                label="Фамилия"
+                {...register('second_name')}
+                error={!!errors.second_name}
+                helperText={errors.second_name?.message}
+                fullWidth
+              />
 
-          <Grid item mb={2}>
-            <TextField
-              type="text"
-              label="Email"
-              {...register('email')}
-              error={!!errors.email}
-              helperText={errors.email?.message}
-              fullWidth
-            />
-          </Grid>
+              <TextField
+                type="text"
+                label="Email"
+                {...register('email')}
+                error={!!errors.email}
+                helperText={errors.email?.message}
+                fullWidth
+              />
 
-          <Grid item mb={2}>
-            <TextField
-              type="text"
-              label="Телефон"
-              {...register('phone')}
-              error={!!errors.phone}
-              helperText={errors.phone?.message}
-              fullWidth
-            />
-          </Grid>
+              <TextField
+                type="text"
+                label="Телефон"
+                {...register('phone')}
+                error={!!errors.phone}
+                helperText={errors.phone?.message}
+                fullWidth
+              />
 
-          <Grid item mb={2}>
-            <TextField
-              type="text"
-              label="Логин"
-              {...register('login')}
-              error={!!errors.login}
-              helperText={errors.login?.message}
-              fullWidth
-            />
-          </Grid>
+              <TextField
+                type="text"
+                label="Логин"
+                {...register('login')}
+                error={!!errors.login}
+                helperText={errors.login?.message}
+                fullWidth
+              />
 
-          <Grid item mb={2}>
-            <TextField
-              type="password"
-              label="Пароль"
-              fullWidth
-              {...register('password')}
-              error={!!errors.password}
-              helperText={errors.password?.message}
-            />
-          </Grid>
+              <TextField
+                type="password"
+                label="Пароль"
+                fullWidth
+                {...register('password')}
+                error={!!errors.password}
+                helperText={errors.password?.message}
+              />
+            </Stack>
+          </CardContent>
+          <CardActions>
+            <Stack direction="column" width="100%" spacing={3}>
+              <Button
+                variant="contained"
+                type="submit"
+                size="medium"
+                fullWidth
+                disabled={!isValid}
+                sx={{
+                  borderRadius: '10px',
+                }}
+              >
+                Регистрация
+              </Button>
 
-          <Grid item mb={3}>
-            <Button
-              variant="contained"
-              type="submit"
-              size="medium"
-              fullWidth
-              disabled={!isValid}
-              sx={{
-                borderRadius: '10px',
-              }}
-            >
-              Регистрация
-            </Button>
-          </Grid>
-
-          <Typography fontSize="15px" fontWeight="700" align="center">
-            <Link href={SigninPagePath.path} underline="none">
-              Войти
-            </Link>
-          </Typography>
+              <Typography fontSize={15} fontWeight={700} align="center">
+                <Link href={SigninPagePath.path} underline="none">
+                  Войти
+                </Link>
+              </Typography>
+            </Stack>
+          </CardActions>
         </form>
-      </Grid>
+      </Card>
     </Grid>
   );
 };
