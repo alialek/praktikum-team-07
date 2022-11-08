15743d11bb102a13bf36d067cbd6a40ab5d73bf4
Sergey Vlasov <sidodji1337@yandex.ru>
2022-11-08 21:36:40 +0300

    PRA-65 fix: some fix

--- packages/client/src/layouts/Default/Default.tsx
+++ packages/client/src/layouts/Default/Default.tsx
@@ -4,12 +4,12 @@ import Container from '@mui/material/Container';
 import { Header } from '@/components/Header';
 
 export const Default = () => (
-  <React.Fragment>
+  <>
     <Header />
     <Container maxWidth="lg" sx={{ py: 6 }}>
       <main>
         <Outlet />
       </main>
     </Container>
-  </React.Fragment>
+  </>
 );
--- packages/client/src/pages/SigninPage/SigninPage.tsx
+++ packages/client/src/pages/SigninPage/SigninPage.tsx
@@ -1,23 +1,41 @@
 import React from 'react';
-import { SignupPagePath } from '@/router/paths';
-import { SigninInputModel } from '../../models/auth.model'
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { useForm } from 'react-hook-form';
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { yupResolver } from '@hookform/resolvers/yup';
-import {Link, Button, TextField, Typography, Card, Box, Grid, Stack, CardContent, CardActions} from '@mui/material'
-import { REQUIRED_VALUE_MESSAGE, EMAIL_VALUE_MESSAGE } from "@/сonstants/validationMessages";
-import * as FormValidate from '../../utils/formValidation'
-
+import {
+  Link,
+  Button,
+  TextField,
+  Typography,
+  Card,
+  Box,
+  Grid,
+  Stack,
+  CardContent,
+  CardActions,
+} from '@mui/material';
+import { SigninInputModel } from '@/models/auth.model';
+import { SignupPagePath } from '@/router/paths';
+import {
+  REQUIRED_VALUE_MESSAGE,
+  EMAIL_VALUE_MESSAGE,
+} from '@/сonstants/validationMessages';
+import * as FormValidate from '../../utils/formValidation';
 
 export const SigninPage: React.FC = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<SigninInputModel>({
-    resolver: yupResolver(FormValidate.loginFormValidationSchema)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SigninInputModel>({
+    resolver: yupResolver(FormValidate.loginFormValidationSchema),
   });
   const onSubmit = (data: SigninInputModel) => console.log(data);
   return (
-    
-    <Box 
+    <Box
       component="div"
-      sx={{maxWidth: 552, ml: 'auto', mr: 'auto', transform: 'scale(0.8)'}}
+      sx={{ maxWidth: 552, ml: 'auto', mr: 'auto', transform: 'scale(0.8)' }}
     >
       <Grid
         container
@@ -25,24 +43,31 @@ export const SigninPage: React.FC = () => {
         direction="column"
         alignItems="center"
         justifyContent="center"
-        style={{ minHeight: '100vh'}}
+        style={{ minHeight: '100vh' }}
       >
         <Grid item xs={3}>
           <form onSubmit={handleSubmit(onSubmit)}>
-            <Card sx={{ minWidth: 552, padding: '50px 40px', borderRadius: '10px', backgroundColor: '#FAFAFA'}}>
-              
+            <Card
+              sx={{
+                minWidth: 552,
+                padding: '50px 40px',
+                borderRadius: '10px',
+                backgroundColor: '#FAFAFA',
+              }}
+            >
               <CardContent>
-                <Typography 
-                  fontSize={24} 
-                  fontWeight="600" 
+                <Typography
+                  fontSize={24}
+                  fontWeight="600"
                   padding="0 0 24px 0"
-                  align="center">
+                  align="center"
+                >
                   Вход
                 </Typography>
-          
+
                 <Stack direction="column" spacing={4}>
-                  <TextField             
-                    {...register("login")}
+                  <TextField
+                    {...register('login')}
                     id="email"
                     label="Email"
                     placeholder="Email"
@@ -50,7 +75,7 @@ export const SigninPage: React.FC = () => {
                     variant="outlined"
                     error={!!errors?.login}
                     helperText={errors.login && EMAIL_VALUE_MESSAGE}
-                    />
+                  />
 
                   <TextField
                     type="password"
@@ -59,45 +84,39 @@ export const SigninPage: React.FC = () => {
                     placeholder="Пароль"
                     error={!!errors?.password}
                     helperText={errors.password && REQUIRED_VALUE_MESSAGE}
-                    multiline 
-                    
-                    {...register("password")}/>
+                    multiline
+                    {...register('password')}
+                  />
 
-
-                    <Typography
-                      variant="caption"
-                      fontSize={18} 
-                      fontWeight="500"
-                      align="center"
-                    >
-                      <Link 
-                        href={SignupPagePath.path}
-                        underline="none">
-                        Нет аккаунта? Регистрация</Link>
-                      
-                    </Typography>
-                
+                  <Typography
+                    variant="caption"
+                    fontSize={18}
+                    fontWeight="500"
+                    align="center"
+                  >
+                    <Link href={SignupPagePath.path} underline="none">
+                      Нет аккаунта? Регистрация
+                    </Link>
+                  </Typography>
                 </Stack>
               </CardContent>
               <CardActions>
-                <Button 
+                <Button
                   type="submit"
-                  size="large" 
+                  size="large"
                   variant="contained"
                   fullWidth
-                    sx={{
-                      borderRadius: '10px'}}
-                  >
-                    Войти
+                  sx={{
+                    borderRadius: '10px',
+                  }}
+                >
+                  Войти
                 </Button>
               </CardActions>
-              
-            </Card>  
+            </Card>
           </form>
         </Grid>
       </Grid>
     </Box>
-   
-
-  )
-}
+  );
+};
