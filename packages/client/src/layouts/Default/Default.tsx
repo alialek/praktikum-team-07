81c16f6b4a8e0e92b10ed1cf9c5eff1b9f293ec9
Sergey Vlasov <sidodji1337@yandex.ru>
2022-11-13 21:34:30 +0300

    rebase

--- packages/client/src/layouts/Default/Default.tsx
+++ packages/client/src/layouts/Default/Default.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container';
 import { Header } from '@/components/Header';
 
-export const Default: React.FC = () => (
+export const Default = () => (
   <React.Fragment>
     <Header />
     <Container maxWidth="lg" sx={{ py: 6 }}>
