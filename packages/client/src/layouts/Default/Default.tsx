8f38a150c922dd6e52f1adc7e8bbc39594da19fe
Sergey Vlasov <sidodji1337@yandex.ru>
2022-11-15 01:05:42 +0300

    PRA-56 feature: Смена БГ

--- packages/client/src/layouts/Default/Default.tsx
+++ packages/client/src/layouts/Default/Default.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container';
+import { useTheme } from '@mui/material/styles';
 import { Header } from '@/components/Header';
+import { mainStyles } from './Style';
 
-export const Default = () => (
-  <>
-    <Header />
-    <Container maxWidth="lg" sx={{ py: 6 }}>
-      <main>
-        <Outlet />
-      </main>
-    </Container>
-  </>
-);
+export const Default = () => {
+  const theme = useTheme();
+  const { mode } = theme.palette;
+  return (
+    <>
+      <Header />
+      <Container sx={mode === 'light' ? mainStyles.main : mainStyles.mainDark}>
+        <main>
+          <Outlet />
+        </main>
+      </Container>
+    </>
+  );
+};
