982f6b03ee2b36582633ca9ee732a4a68f34b48c
alina <denisova@logema.org>
2022-11-17 21:46:55 +0300

    PRA-39 Убрала фрагмент

--- packages/client/src/pages/EndGamePage/EndGamePage.tsx
+++ packages/client/src/pages/EndGamePage/EndGamePage.tsx
@@ -10,41 +10,39 @@ export const EndGamePage = () => {
   const { pathname } = useLocation();
 
   return (
-    <>
-      <Box sx={endGameStyles.boxWrapper}>
-        <Grid
-          container
-          spacing={2}
-          justifyContent="center"
-          alignItems="center"
-          direction="column"
-        >
-          <Box sx={endGameStyles.welcomeBox}>
-            <Grid sx={endGameStyles.wrapperCount}>
-              <Typography variant="h6">{COUNT_TEXT}</Typography>
-              <Typography variant="h6">{RECORD_TEXT}</Typography>
-            </Grid>
-            <Grid sx={endGameStyles.wrapperTitle}>
-              <Typography sx={endGameStyles.title} variant="h3">
-                {END_TEXT}
-                <Box sx={endGameStyles.img} />
-              </Typography>
-            </Grid>
-          </Box>
-          <Grid item xs={12}>
-            <Button
-              variant="contained"
-              color="primary"
-              startIcon={<PlayArrowIcon />}
-              component={Link}
-              to={RootPath.path}
-              state={{ previousPath: pathname }}
-            >
-              {START_GAME_TEXT}
-            </Button>
+    <Box sx={endGameStyles.boxWrapper}>
+      <Grid
+        container
+        spacing={2}
+        justifyContent="center"
+        alignItems="center"
+        direction="column"
+      >
+        <Box sx={endGameStyles.welcomeBox}>
+          <Grid sx={endGameStyles.wrapperCount}>
+            <Typography variant="h6">{COUNT_TEXT}</Typography>
+            <Typography variant="h6">{RECORD_TEXT}</Typography>
           </Grid>
+          <Grid sx={endGameStyles.wrapperTitle}>
+            <Typography sx={endGameStyles.title} variant="h3">
+              {END_TEXT}
+              <Box sx={endGameStyles.img} />
+            </Typography>
+          </Grid>
+        </Box>
+        <Grid item xs={12}>
+          <Button
+            variant="contained"
+            color="primary"
+            startIcon={<PlayArrowIcon />}
+            component={Link}
+            to={RootPath.path}
+            state={{ previousPath: pathname }}
+          >
+            {START_GAME_TEXT}
+          </Button>
         </Grid>
-      </Box>
-    </>
+      </Grid>
+    </Box>
   );
 };
