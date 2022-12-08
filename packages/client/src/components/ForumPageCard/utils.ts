0465b354b12610e4bd716827204dc470a066474b
Ekaterina <ches.ekaterina2010@yandex.ru>
2022-12-08 20:55:47 +0300

    fix forum

--- packages/client/src/components/ForumPageCard/utils.ts
+++ packages/client/src/components/ForumPageCard/utils.ts
@@ -1,6 +1,6 @@
-import { ForumItems } from '../Forum/types';
+import { ForumItem } from '../Forum/types';
 
-export const forumsList: ForumItems[] = [
+export const forumsList: ForumItem[] = [
   {
     id: 1,
     title: '123',
@@ -9,21 +9,21 @@ export const forumsList: ForumItems[] = [
     total_answer: 1000,
   },
   {
-    id: 1,
+    id: 2,
     title: '213',
     description: 'sun shine reggae',
     threads: 333,
     total_answer: 600,
   },
   {
-    id: 1,
+    id: 3,
     title: '321',
     description: 'sun shine reggae',
     threads: 4444,
     total_answer: 1111,
   },
   {
-    id: 1,
+    id: 4,
     title: '421',
     description: 'sun shine reggae',
     threads: 100,
