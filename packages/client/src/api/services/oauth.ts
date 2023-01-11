79dab1153b3ca00e8fa8ccfb1384d98272fa2273
Ekaterina Chesnakova <111084605+chesKaterina@users.noreply.github.com>
2023-01-11 13:46:22 +0300

    Feature/pra 50 (#32)
    * add yandex ID button
    
    * oauth authorization
    
    * oauth
    
    * add oauth
    
    * fix button error
    
    * fixed yabutton color
    
    * try fix localhost
    
    * try fix localhost 2
    
    * try to fix localhost
    
    * return localhost(yap app requests) del ls.clear
    
    * Oauth final version

---
+++ packages/client/src/api/services/oauth.ts
@@ -0,0 +1,13 @@
+import { OauthSingInModel } from '@/models/oauth.model';
+import { REDIRECT_URI } from '@/сonstants/main';
+import { api, ApiResponse } from '../client';
+
+export const OauthService = {
+  signin(data: OauthSingInModel): Promise<ApiResponse> {
+    return api.post('/oauth/yandex', data);
+  },
+
+  getServiceId(): Promise<ApiResponse<OauthSingInModel>> {
+    return api.get(`/oauth/yandex/service-id?redirect_uri=${REDIRECT_URI}`);
+  },
+};
