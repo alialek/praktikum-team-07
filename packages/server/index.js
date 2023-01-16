0fcf4cebf2ab6d64a703474733fb66777568944a
VladyKoo <vladykoo@mail.ru>
2023-01-17 01:55:48 +0400

    test

---
+++ packages/server/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const host = 'localhost';
+const port = 3001;
+
+const requestListener = function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.writeHead(200);
+  res.end(JSON.stringify({ status: 'ok', time: new Date() }));
+};
+
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
