8485d145c2a9b99e4620e6601b5e86af425b56ba
VladyKoo <vladykoo@mail.ru>
2023-01-17 21:20:52 +0400

    remove comments

--- packages/client/src/components/App/App.test.tsx
+++ packages/client/src/components/App/App.test.tsx
@@ -1,6 +1,4 @@
-// import * as React from 'react';
 import { screen } from '@testing-library/react';
-// import { App } from './App';
 
 const testId = 'test-root';
 
@@ -9,6 +7,5 @@ global.fetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve('hey'
 
 /* eslint jest/no-disabled-tests: off */
 test.skip('Example test', async () => {
-  // render(<App />);
   expect(screen.getByTestId(testId)).toBeDefined();
 });
