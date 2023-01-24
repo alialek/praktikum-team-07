69cb338c8dda0a44a223f4f165851f030cbfa97c
Vladislav Kurochkin <70705849+VladyKoo@users.noreply.github.com>
2023-01-25 00:34:12 +0400

    PRA-87: fix csp image-src (#49)

--- packages/client/src/сonstants/main.ts
+++ packages/client/src/сonstants/main.ts
@@ -1,13 +1,17 @@
 import { window } from '@/utils/ssrWindow';
 
-const { VITE_DEV_API_URL, VITE_API_URL, VITE_API_PATH } = import.meta.env;
-
 export const IS_DEV = process.env.NODE_ENV !== 'production';
 
 export const YANDEX_API_URL = 'https://ya-praktikum.tech/api/v2';
+
+export const DEV_API_URL = import.meta.env.VITE_DEV_API_URL || 'http://localhost:3001';
+export const PROD_API_URL =
+  import.meta.env.VITE_API_URL || 'https://cloud-atom.ya-praktikum.tech';
+export const API_PATH = import.meta.env.VITE_API_PATH || '/api/v1';
+
 export const API_URL = IS_DEV
-  ? `${VITE_DEV_API_URL}${VITE_API_PATH}`
-  : `${VITE_API_URL}${VITE_API_PATH}` || '/api/v1';
+  ? `${DEV_API_URL}${API_PATH}`
+  : `${PROD_API_URL}${API_PATH}`;
 
 export const UPDATE_PROFILE_URL = '/user/profile';
 export const GET_AVATAR_URL = `${YANDEX_API_URL}/resources`;
--- packages/server/src/configs/csp/policies/core.ts
+++ packages/server/src/configs/csp/policies/core.ts
@@ -1,9 +1,11 @@
-import { EVAL, INLINE, SELF, CSPDirectives } from 'csp-header';
+import { EVAL, INLINE, SELF, CSPDirectives, BLOB } from 'csp-header';
+
+import { API_URL, YANDEX_API_URL } from '@/constants/main';
 
 export const corePolicies: Partial<CSPDirectives> = {
   'connect-src': [SELF],
   'default-src': [SELF],
   'script-src': [EVAL, INLINE],
   'style-src': [INLINE],
-  'img-src': [SELF, INLINE],
+  'img-src': [SELF, INLINE, BLOB, YANDEX_API_URL, API_URL],
 };
