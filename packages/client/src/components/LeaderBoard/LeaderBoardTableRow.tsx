309c45166085763d8542c23534fff616426b5eae
Sergey Vlasov <sidodji1337@yandex.ru>
2023-01-25 20:05:43 +0300

    fix Проверка на null в user_name в Лидерборде

--- packages/client/src/components/LeaderBoard/LeaderBoardTableRow.tsx
+++ packages/client/src/components/LeaderBoard/LeaderBoardTableRow.tsx
@@ -12,11 +12,14 @@ export const LeaderBoardTableRow: React.FC<LeaderBoardTableRowProps> = ({ rows }
       {rows.map((row: LeaderboardData, index) => (
         <TableRow hover key={index}>
           <TableCell>
-            <Avatar src={row.data.avatar} alt={row.data.user_name}>
-              {row.data.user_name.charAt(0)}
+            <Avatar
+              src={row.data.avatar}
+              alt={row.data.user_name ? row.data.user_name : 'Anonymous'}
+            >
+              {row.data.user_name ? row.data.user_name.charAt(0) : 'A'}
             </Avatar>
           </TableCell>
-          <TableCell>{row.data.user_name}</TableCell>
+          <TableCell>{row.data.user_name ? row.data.user_name : 'Anonymous'}</TableCell>
           <TableCell>{row.data.score}</TableCell>
         </TableRow>
       ))}
